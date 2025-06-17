7c919dd9078088ffc54c2e2cc1e173d122c4e779
wright2002 <kyle.m.wright1@gmail.com>
2025-06-17 16:27:06 -0400

    database query template

---
+++ db_connect_template.py
@@ -0,0 +1,23 @@
+import os
+import pandas as pd
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get DB credentials from env
+username = os.getenv('PGUSER')
+password = os.getenv('PGPASSWORD')
+host = os.getenv('PGHOST', 'localhost')
+port = os.getenv('PGPORT', '5432')
+database = os.getenv('PGDATABASE')
+
+# Create engine and query
+engine = create_engine(f'postgresql://{username}:{password}@{host}:{port}/{database}')
+query = "SELECT * FROM Session_ctu_malware_1_1;"
+df = pd.read_sql_query(query, engine)
+
+# INSERT LOGIC HERE
+
+print(df.head())
